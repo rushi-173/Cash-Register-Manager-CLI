676257be13c910bf7e218fa6936cbb0618781ab9
Rushikesh Chaudhari <rushi.chaudhari@yahoo.com>
2020-12-22 12:26:49 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const readlineSync = require("readline-sync");
+//data
+const currencies = [1,2,5,10,20,50,100,500,2000]
+const output = [0,0,0,0,0,0,0,0,0]
+
+//input
+function getAmt(){
+  let billAmt;
+  billAmt = readlineSync.question("Enter bill amount : ")
+  return billAmt;
+}
+let billAmt = getAmt();
+//processing 
+
+//output
+console.log(billAmt)
